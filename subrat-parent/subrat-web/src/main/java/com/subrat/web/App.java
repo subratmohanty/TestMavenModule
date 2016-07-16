8bd4df6e42ac20da06be41ac30974af660e757cf
subratmohanty <subratmohanty23@gmail.com>
2016-07-16 13:31:27 +0530

    Adding Web file

---
+++ subrat-parent/subrat-web/src/main/java/com/subrat/web/App.java
@@ -0,0 +1,13 @@
+package com.subrat.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
